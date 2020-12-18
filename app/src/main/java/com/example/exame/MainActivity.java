0a3e1cb7e2be26330d041fd46218516c3f74065d
Carlos Daniel <danielmangaxd@gmail.com>
2020-12-17 21:40:57 -0300

    Terceiro commit

--- app/src/main/java/com/example/exame/MainActivity.java
+++ app/src/main/java/com/example/exame/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.exame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+
+    private List<Cachorro> listaCachorros;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        listaCachorros = new ArrayList<>();
+
     }
 }
